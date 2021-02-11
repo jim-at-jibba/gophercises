45f873e17a43801a5a5a0c5ecc00c97267cb473d
James Best <jim@justjibba.net>
2021-02-11 02:27:24 +0000

    Add timer to exercise 1

--- exercise1/main.go
+++ exercise1/main.go
@@ -3,19 +3,18 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
-	"errors"
 	"flag"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 //Number of Questions to ask
 const totalQuestions = 5
 
-type Question struct {
+type question struct {
 	question string
 	answer   string
 }
@@ -27,38 +26,43 @@ func main() {
 	}
 }
 
-func readArguments() string {
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
+func readArguments() (string, int) {
 
 	fileName := flag.String("filename", "problems.csv", "quiz question file name")
+	timeLimit := flag.Int("timeLimit", 30, "Time allowed for quiz")
 
 	flag.Parse()
 
 	// returning actual value as flag package returns a pointer
-	return *fileName
+	return *fileName, *timeLimit
 }
 
 func openFile(filename string) (io.Reader, error) {
 	return os.Open(filename)
 }
 
-func readCSV(f io.Reader) ([]Question, error) {
+func readCSV(f io.Reader) ([]question, error) {
 	//	i. read csv
 	csvR := csv.NewReader(f)
 
+	// we can confidently use readAll because it is unlikely there
+	// are gonna be millions of lines
+	lines, err := csvR.ReadAll()
+
+	if err != nil {
+		exit("error reading csv")
+	}
 	//  ii. prepare questions slice
-	questions := make([]Question, len())
-	for {
-		record, err := csvR.Read()
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if record[0] != "" && record[1] != "" {
-			questions = append(questions, Question{record[0], record[1]})
+	questions := make([]question, len(lines))
+	for i, line := range lines {
+		questions[i] = question{
+			question: line[0],
+			answer:   line[1],
 		}
 	}
 	return questions, nil
@@ -69,23 +73,24 @@ func getInput(input chan string) {
 		in := bufio.NewReader(os.Stdin)
 		result, err := in.ReadString('\n')
 		if err != nil {
-			log.Fatal(err)
+			exit(err.Error())
 		}
 
 		input <- result
 	}
 }
 
-func askQuestions(questions []Question) (int, error) {
+func askQuestions(questions []question, timeLimit int) (int, error) {
 	totalScore := 0
+	timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
 
 	done := make(chan string)
 
 	go getInput(done)
 
 	for i := range [totalQuestions]int{} {
-		ans, err := askQuestion(questions[i].question, questions[i].answer, done)
-		if err != nil {
+		ans, err := askQuestion(questions[i].question, questions[i].answer, done, timer.C)
+		if err != nil && ans == -1 {
 			return totalScore, nil
 		}
 		totalScore += ans
@@ -93,10 +98,12 @@ func askQuestions(questions []Question) (int, error) {
 	return totalScore, nil
 }
 
-func askQuestion(question string, answer string, done <-chan string) (int, error) {
+func askQuestion(question string, answer string, done <-chan string, timer <-chan time.Time) (int, error) {
 	fmt.Printf("Question: %v\n", question)
 	for {
 		select {
+		case <-timer:
+			return -1, fmt.Errorf("Time out")
 		case ans := <-done:
 			score := 0
 			if strings.Compare(strings.Trim(strings.ToLower(ans), "\n"), answer) == 0 {
@@ -114,30 +121,30 @@ func run() error {
 	fmt.Printf("Yay - cooking on gas\n")
 
 	// 1. read args
-	fileName := readArguments()
+	fileName, timeLimit := readArguments()
 
 	// 2. Open file
 	f, err := openFile(fileName)
 
 	if err != nil {
-		return err
+		exit(err.Error())
 	}
 
 	// 3. Read csv
 	questions, err := readCSV(f)
 
 	if err != nil {
-		return errors.New("Error :: problem reading CSV")
+		exit("Error :: problem reading CSV")
 	}
 
 	if questions == nil {
-		return errors.New("Error :: No Questions")
+		exit("Error :: No Questions")
 	}
 	// 4. askQuestions
-	score, err := askQuestions(questions)
+	score, err := askQuestions(questions, timeLimit)
 
 	if err != nil {
-		return errors.New("Error :: Problem asking questions")
+		exit("Error :: Problem asking questions")
 	}
 
 	// 5. return results
